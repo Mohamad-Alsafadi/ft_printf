b8d4f578b2a04b79b50b5818458ff982e4869941
Mohamad-Alsafadi <141864189+Mohamad-Alsafadi@users.noreply.github.com>
2025-07-19 18:25:40 +0200

    ft_printf.c

--- ft_printf.c
+++ ft_printf.c
@@ -6,15 +6,15 @@
 /*   By: malsafad <malsafad@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/03 19:57:39 by malsafad          #+#    #+#             */
-/*   Updated: 2025/07/11 17:26:34 by malsafad         ###   ########.fr       */
+/*   Updated: 2025/07/19 18:19:34 by malsafad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int	ft_format(char spec, va_list ap)
+int ft_format(char spec, va_list ap)
 {
-	int	count;
+	int count;
 
 	count = 0;
 	if (spec == 'c')
@@ -38,11 +38,12 @@ int	ft_format(char spec, va_list ap)
 	return (count);
 }
 
-int	ft_printf(const char *format, ...)
+int ft_printf(const char *format, ...)
 {
-	va_list	ap;
-	int		count;
-
+	va_list ap;
+	int count;
+	if (!format)
+		return (0);
 	va_start(ap, format);
 	count = 0;
 	while (*format != '\0')
